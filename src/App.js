f279cd176bfa7d60b54d928e5443fecd6af2ef1c
akanksha-d-23 <140509405+akanksha-d-23@users.noreply.github.com>
2023-10-28 13:23:39 +0530

    management app complete

--- src/App.js
+++ src/App.js
@@ -1,24 +1,66 @@
-import logo from './logo.svg';
+import React, {useEffect , useState} from 'react';
 import './App.css';
 
 function App() {
+  const [data, setData] = useState([])
+  useEffect(()=>{
+    fetch("https://jsonplaceholder.typicode.com/users").then((result)=>{
+      result.json().then((resp)=>{
+        setData(resp)
+      })
+    })
+  },[])
   return (
+    <>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='container' style={{color : 'darkcyan'}}>
+        <h2 style={{color : 'darkcyan'} }>Users Data</h2>
+        <table className='table table-bordered'style={{color : 'darkindigo'}}>
+        <thead>
+        <tr className='col-md-6'>
+          <th>ID</th>
+          <th>Name</th>
+          <th>Username</th>
+          <th>Email</th>
+          <th>Street</th>
+          <th>Suite</th>
+          <th>City</th>
+          <th>Zipcode</th>
+          <th>Latitude</th>
+          <th>Longitude</th>
+          <th>Phone</th>
+          <th>Website</th>
+          <th>Company Name</th>
+          <th>Catch Phrase</th>
+          <th>Business</th>
+        </tr>
+      </thead>
+      <tbody className='col-md-6'>
+        { data.map((userData)=>
+        <tr className='col-md-6'>
+          <td className='col-md-6'>{userData.id}</td>
+          <td className='col-md-6'>{userData.name}</td>
+          <td className='col-md-6'>{userData.username}</td>
+          <td className='col-md-6'>{userData.email}</td>
+          <td className='col-md-6'>{userData.address.street}</td>
+          <td className='col-md-6'>{userData.address.suite}</td>
+          <td className='col-md-6'>{userData.address.city}</td>
+          <td className='col-md-6'>{userData.address.zipcode}</td>
+          <td className='col-md-6'>{userData.address.geo.lat}</td>
+          <td className='col-md-6'>{userData.address.geo.lng}</td>
+          <td className='col-md-6'>{userData.phone}</td>
+          <td className='col-md-6'>{userData.website}</td>
+          <td className='col-md-6'>{userData.company.name}</td>
+          <td className='col-md-6'>{userData.company.catchPhrase}</td>
+          <td className='col-md-6'>{userData.company.bs}</td>
+        </tr>
+
+)}
+      </tbody>
+        </table>
+        </div>
     </div>
+    </>
   );
 }
 
